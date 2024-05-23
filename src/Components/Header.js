fb05648be25d72c50dc9e2762d52124795073e7e
pwtansg <pwtan.edu@gmail.com>
2024-05-23 19:56:02 +0800

    Add logout feature

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,38 +1,69 @@
-import { FaSignInAlt, FaSignOutAlt, FaHome, FaUser } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { FaSignInAlt, FaSignOutAlt, FaHome, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { logout } from '../Services/cognitoAuth';
+import { resetUserSession, getSessionUser } from '../Services/userSession';
 
 const Header = () => {
-    // const navigate = useNavigate()
-    const logout = () => {
-        // localStorage.removeItem('user')
-        // navigate('/login')
+    const currentUser = getSessionUser();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const onLogout = () => {
+        resetUserSession()
+        logout();
+        navigate('/login')
     }
-
 
     return (
         <header className='header'>
-            <div className=''>
+            {/* <div className=''>
                 <Link to='/'>
                     <span className='icon-white'>
                         <FaHome />
                     </span>
                 </Link>
-            </div>
+            </div> */}
             <ul>
-                <li>
+                {location.pathname !== '/login' && <li>
                     <Link to='/'>
+                        <span className='icon-white'>
+                            <FaHome />
+                        </span>
+                    </Link>
+                </li>}
+            </ul>
+            <ul>
+                {/* <li>
+                    <Link to='/login'>
                         <span className='icon-white'>
                             <FaSignInAlt />
                         </span>
                     </Link>
+                </li> */}
+                {location.pathname !== '/login' && (
+                    <>
+                        <li>
+                            {currentUser ? currentUser : ""}
+                        </li>
+                        <li>
+                            <Link to='/login'>
+                                <span className='icon-white' onClick={onLogout}>
+                                    <FaSignOutAlt />
+                                </span>
+                            </Link>
+                        </li></>
+                )}
+                {/* <li>
+                    {auth?.user?.username ? auth.user?.username ? auth.user.username : "" : ""}
                 </li>
                 <li>
-                    <div onClick={logout}>
-                        <FaSignOutAlt />
-                    </div>
-                </li>
-                
+                    {isAuthenticated &&
+                        <div onClick={logout}>
+                            <FaSignOutAlt />
+                        </div>
+                    }
+                </li> */}
+
                 {/* <li>
                     <Link to='/register'>
                         <FaUser />
