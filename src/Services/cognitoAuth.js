2f397f088364d2571a19dd51b8109dc2a1ae8142
pwtansg <pwtan.edu@gmail.com>
2024-05-23 18:42:13 +0800

    Add aws cognito auth

---
+++ src/Services/cognitoAuth.js
@@ -0,0 +1,34 @@
+import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
+import userpool from '../userpools';
+
+
+export const authenticate = (Email, Password) => {
+    return new Promise((resolve, reject) => {
+        const user = new CognitoUser({
+            Username: Email,
+            Pool: userpool
+        });
+
+        const authDetails = new AuthenticationDetails({
+            Username: Email,
+            Password
+        });
+
+        user.authenticateUser(authDetails, {
+            onSuccess: (result) => {
+                console.log("login successful");
+                resolve(result);
+            },
+            onFailure: (err) => {
+                console.log("login failed", err);
+                reject(err);
+            }
+        });
+    });
+};
+
+export const logout = () => {
+    const user = userpool.getCurrentUser();
+    // console.log(user)
+    user.signOut();
+};
