d4534655c35b7bf3365b6bfcf26e686913040c67
pwtansg <pwtan.edu@gmail.com>
2024-05-28 13:09:43 +0800

    AuthContext - fix handling of persistent data upon page refresh

--- src/Context/Auth/AuthProvider.js
+++ src/Context/Auth/AuthProvider.js
@@ -1,9 +1,18 @@
 import { createContext, useState } from 'react'
+import { getSessionUser, getSessionToken } from '../../Services/userSession';
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({})
+
+    //handle persist data on page refresh
+    const getInitialState = () => {
+        const username = getSessionUser();
+        const token = getSessionToken()
+        return username && token ? { username, token } : {}
+    }
+
+    const [auth, setAuth] = useState(getInitialState)
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
